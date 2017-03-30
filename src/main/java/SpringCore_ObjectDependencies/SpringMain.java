2b0b0d671b68251f33de42418db912d091f445fd
Rolando Colon <rolando_colon@homedepot.com>
2017-03-30 16:53:12 -0400

    Adding Day 4 Spring Core Topics 1. Creating Bean with ref for property and contructor

---
+++ src/main/java/SpringCore_ObjectDependencies/SpringMain.java
@@ -0,0 +1,17 @@
+package SpringCore_ObjectDependencies;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+/**
+ * Created by RXC8414 on 3/30/2017.
+ */
+public class SpringMain {
+    public static void main(String[] args) {
+        ApplicationContext appContext = new FileSystemXmlApplicationContext("src\\main\\resources\\appContextObjectDependencies.xml");
+        ClassC myClassC = appContext.getBean("classC",ClassC.class);
+        ClassC myClassCWithConst = appContext.getBean("classCWithConstructor",ClassC.class);
+        myClassC.identifyClass();
+        myClassCWithConst.identifyClass();
+    }
+}
